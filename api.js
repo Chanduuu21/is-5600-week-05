75d2add7e2ef367bf36c90ce817c0dce9c277b87
Chandra Sai Ettneni <chanduuu21@gmail.com>
2025-11-24 02:45:49 +0000

    week5

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const Products = require('./products')
+const Orders = require('./orders')
 const autoCatch = require('./lib/auto-catch')
 
 /**
@@ -45,13 +46,14 @@ async function getProduct(req, res, next) {
 }
 
 /**
- * Create a product
- * @param {object} req 
- * @param {object} res 
+ * Create a new product
+ * @param {object} req
+ * @param {object} res
+ * @param {NextFunction} next
  */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
+async function createProduct (req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
 /**
@@ -75,11 +77,37 @@ async function deleteProduct(req, res, next) {
   res.json({ success: true })
 }
 
+/**
+ * Create an order
+ */
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(order)
+}
+
+/**
+ * List orders
+ */
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({
+    offset: Number(offset),
+    limit: Number(limit),
+    productId,
+    status
+  })
+
+  res.json(orders)
+}
+
 module.exports = autoCatch({
   handleRoot,
   listProducts,
   getProduct,
   createProduct,
+  createOrder,
+  listOrders,
   editProduct,
   deleteProduct
 });
